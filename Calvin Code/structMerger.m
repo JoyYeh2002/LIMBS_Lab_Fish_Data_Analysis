24379dcf214720a8509bd810640d5467d1e86f9f
cyeh2003 <136770767+cyeh2003@users.noreply.github.com>
2023-07-06 16:25:48 -0700

    Delete Calvin Code directory

--- Calvin Code/structMerger.m
+++
@@ -1,9 +0,0 @@
-load("newStruct.mat")
-trial = cell2mat(group(3).fishData(11).xClean02Tr)
-rep = cell2mat(group(3).fishData(11).xClean02Rep);
-orientation = [-1,-1,-1,-1,-1,-1,-1,1,1,1,1,1,1];
-
-c = [trial; rep; orientation]
-group(3).fishData(11).merged = c;
-save('newStruct.mat', 'group', '-mat')
-
